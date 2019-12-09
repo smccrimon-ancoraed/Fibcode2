2c0929cfd0cd90bb6dab2a979d18c267ab3c7196
smccrimon-ancoraed <stefon.mccrimon@faculty.ancored.com>
2019-12-09 08:57:41 -0800

    Push changes on code

--- Fibcode2/Program.cs
+++ Fibcode2/Program.cs
@@ -4,9 +4,42 @@ namespace Fibcode2
 {
     class Program
     {
+
+
+   /*
+         * Function:  int Fib(n)
+         * Return nth terms of the Fibonacci series using a Looping method / iterative method
+         * 
+         * 
+         */
+
+        public int Fib(int num)
+        {
+            // Recursive method of calling itself
+            // Call itself until it meets base initials as either  0 to 1 
+
+            if (num == 0) { return 0; }
+            else if (num == 1) { return 1; }
+            else
+            {
+                return Fib(num - 1) + Fib(num - 2);
+            }
+                
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program mynum = new Program();
+
+
+            Console.WriteLine("Fibonacci numbers\n");
+            Console.WriteLine("The number of rabbits paris at the end of n months");
+            Console.WriteLine("\nIn this example we will use n=14\n");
+            Console.WriteLine("Fib(14) = "+mynum.Fib(14)+"\n");
+            Console.ReadKey();
         }
     }
 }
+    
